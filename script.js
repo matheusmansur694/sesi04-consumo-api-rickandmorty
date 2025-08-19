a085f2d237243fae312e32e1637a22d67b2c3446
matheusmansur694 <matheus.toniazzo01093752@sesisenaipr.org.br>
2025-08-19 11:26:43 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-function qualquer(){
-    
+//https://rickandmortyapi.com/api/character
+
+const pageInput = document.getElementById("pageInput");
+const searchBtn = document.getElementById("searchBtn");
+const resultsDIv = document.getElementById("results");
+
+async function fetchCaracters(page){
+    resultsDIv.innerHTML = "<p>Carregando..</p>"
+
+    try {
+        const response = await fetch(`https://rickandmortyapi.com/api/character`)
+        const data = await response.json()
+        console.log(data)
+    } catch (error) {
+       resultsDIv.innerHTML = "<p>Erro ao buscar personagens!</p>" 
+    }
 }
+
+fetchCaracters(1)
